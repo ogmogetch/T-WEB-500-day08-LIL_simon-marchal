3aa9957e3a20044211e8dc5ba6a87b35f57d92a1
simon marchal <simon.marchal@epitech.eu>
2023-10-04 10:51:00 -0400

    change

--- task01.php
+++ task01.php
@@ -1,8 +1,9 @@
 <?php
-function discover_type($name, $age, $isStudent, $gpa = 0.0) {
+
+function discover_type(int $age, string $name, float $gpa, bool $isStudent) {
     if ($isStudent) {
-        echo "Hello my name is $name, I’m $age years old. I’m a student with a GPA of $gpa.\n";
+        echo "Hello my name is $name, I'm $age years old. I'm a student at Epitech with a GPA of $gpa.";
     } else {
-        echo "Hello my name is $name, I’m $age years old. I’m not a student.\n";
+        echo "Hello my name is $name, I'm $age years old. I'm not a student.";
     }
 }
